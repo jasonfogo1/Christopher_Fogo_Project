d67c3b98d13a2b8f5b016a404997fd65664889ab
jasonfogo1 <jasonfog1@gmail.com>
2022-12-12 21:10:17 -0500

    Authentification

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://ui-automation-camp.vercel.app',
+    specPattern: 'cypress/e2e/test/**/*.cy.{js,jsx,ts,tsx}',
+    chromeWebSecurity:false,
   },
 });
